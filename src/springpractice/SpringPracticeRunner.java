307288842f02c4dfe0df8d27d2a11783a159894e
Dmitriy <fed3375546@yandex.ru>
2020-03-07 15:03:24 +0300

    test

--- src/springpractice/SpringPracticeRunner.java
+++ src/springpractice/SpringPracticeRunner.java
@@ -3,8 +3,7 @@ package springpractice;
 public class SpringPracticeRunner {
 
 	public static void main(String[] args) {
-		// git test
-
+		
 		Coach theCoach = new TrackCoach();
 
 		System.out.println(theCoach.getDailyWorkout());
