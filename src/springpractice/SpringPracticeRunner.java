b33265c22205cb4eeef68de64ddf69f89a766416
Dmitriy <fed3375546@yandex.ru>
2020-03-07 16:57:04 +0300

    refactoring

--- src/springpractice/SpringPracticeRunner.java
+++ src/springpractice/SpringPracticeRunner.java
@@ -3,9 +3,8 @@ package springpractice;
 public class SpringPracticeRunner {
 
 	public static void main(String[] args) {
-		
-		Coach theCoach = new TrackCoach();
 
+		Coach theCoach = new TrackCoach();
 		System.out.println(theCoach.getDailyWorkout());
 	}
 
