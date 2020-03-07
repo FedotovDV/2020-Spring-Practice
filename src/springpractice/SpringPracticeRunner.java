69f36a2eb65d833bb8f70602c2b8da186f7eb990
Dmitriy <fed3375546@yandex.ru>
2020-03-07 14:09:27 +0300

    spring practice

---
+++ src/springpractice/SpringPracticeRunner.java
@@ -0,0 +1,12 @@
+package springpractice;
+
+public class SpringPracticeRunner {
+
+	public static void main(String[] args) {
+
+		Coach theCoach = new TrackCoach();
+
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
