d467bcf04be5aae94ee8e12144ce01711136f2c4
Dmitriy <fed3375546@yandex.ru>
2020-03-09 12:35:21 +0300

    prototype scope example

--- src/springpractice/TrackCoach.java
+++ src/springpractice/TrackCoach.java
@@ -1,7 +1,8 @@
 package springpractice;
 
-public class TrackCoach implements Coach {
+import org.springframework.beans.factory.DisposableBean;
 
+public class TrackCoach implements Coach, DisposableBean {
 
 	private FortuneService fortuneService;
 
@@ -21,5 +22,12 @@ public class TrackCoach implements Coach {
 	public String getDailyFortune() {
 		return "Just Do It: " + fortuneService.getFortune();
 	}
-
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: init-method=doMyStartupStuff");
+	}
+	
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("TrackCoach: inside method destroy");		
+	}
 }
