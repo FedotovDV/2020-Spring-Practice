881e16566705555702b0c8a90474b946383daee1
Dmitriy <fed3375546@yandex.ru>
2020-03-15 12:33:51 +0300

    @Autowired setter injection

--- src/lesson07/annotationpractice/TennisCoach.java
+++ src/lesson07/annotationpractice/TennisCoach.java
@@ -3,16 +3,25 @@ package lesson07.annotationpractice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class TennisCoach implements Coach {
 	
 	private FortuneService fortuneService;
-	
-	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constraction!");
 	}
+	
+//	@Autowired
+//	public TennisCoach(FortuneService fortuneService) {
+//	    this.fortuneService = fortuneService;
+//  }
 
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach: inside setFortuneService() method!");
+		this.fortuneService = fortuneService;
+	}
 	@Override
 	public String getDailyWorkout() {
 				return "Practice your backhand volley";
